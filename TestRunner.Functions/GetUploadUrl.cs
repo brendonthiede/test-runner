f5c65ea5250848cde676bcf823674088f32dd8d2
Brendon Thiede <ThiedeB@courts.mi.gov>
2018-11-02 10:17:37 -0400

    Set container permissions when uploading to it

--- TestRunner.Functions/GetUploadUrl.cs
+++ TestRunner.Functions/GetUploadUrl.cs
@@ -8,57 +8,61 @@ using System.Threading.Tasks;
 
 namespace TestRunner.Functions
 {
-    public static class GetUploadUrl
+  public static class GetUploadUrl
+  {
+    [FunctionName("GetUploadUrl")]
+    public static async Task<object> Run(
+        [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req)
     {
-        [FunctionName("GetUploadUrl")]
-        public static async Task<object> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req)
+      var isMissingParameter = false;
+      var errorMessage = "";
+      if (!req.GetQueryParameterDictionary().TryGetValue("filename", out string filenameParameter))
+      {
+        isMissingParameter = true;
+        errorMessage += "\nfilename parameter is required";
+      }
+      if (!req.GetQueryParameterDictionary().TryGetValue("environment", out string environmentParameter))
+      {
+        isMissingParameter = true;
+        errorMessage += "\nenvironment parameter is required";
+      }
+
+      if (isMissingParameter)
+      {
+        return new
         {
-            var isMissingParameter = false;
-            var errorMessage = "";
-            if (!req.GetQueryParameterDictionary().TryGetValue("filename", out string filenameParameter))
-            {
-                isMissingParameter = true;
-                errorMessage += "\nfilename parameter is required";
-            }
-            if (!req.GetQueryParameterDictionary().TryGetValue("environment", out string environmentParameter))
-            {
-                isMissingParameter = true;
-                errorMessage += "\nenvironment parameter is required";
-            }
+          error = errorMessage
+        };
+      }
 
-            if (isMissingParameter)
-            {
-                return new
-                {
-                    error = errorMessage
-                };
-            }
+      var filename = filenameParameter;
+      var environment = environmentParameter;
 
-            var filename = filenameParameter;
-            var environment = environmentParameter;
+      CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+          Environment.GetEnvironmentVariable("WEBSITE_CONTENTAZUREFILECONNECTIONSTRING", EnvironmentVariableTarget.Process));
+      var client = storageAccount.CreateCloudBlobClient();
+      var container = client.GetContainerReference(environment);
+      await container.CreateIfNotExistsAsync();
+      
+      BlobContainerPermissions permissions = await container.GetPermissionsAsync();
+      permissions.PublicAccess = BlobContainerPublicAccessType.Container;
+      await container.SetPermissionsAsync(permissions);
 
-            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
-                Environment.GetEnvironmentVariable("WEBSITE_CONTENTAZUREFILECONNECTIONSTRING", EnvironmentVariableTarget.Process));
-            var client = storageAccount.CreateCloudBlobClient();
-            var container = client.GetContainerReference(environment);
-            await container.CreateIfNotExistsAsync();
+      CloudBlockBlob blob = container.GetBlockBlobReference($"{filename}");
 
-            CloudBlockBlob blob = container.GetBlockBlobReference($"{filename}");
+      SharedAccessBlobPolicy adHocSAS = new SharedAccessBlobPolicy()
+      {
+        SharedAccessExpiryTime = DateTime.UtcNow.AddMinutes(3),
+        Permissions = SharedAccessBlobPermissions.Read | SharedAccessBlobPermissions.Write | SharedAccessBlobPermissions.Create
+      };
 
-            SharedAccessBlobPolicy adHocSAS = new SharedAccessBlobPolicy()
-            {
-                SharedAccessExpiryTime = DateTime.UtcNow.AddMinutes(3),
-                Permissions = SharedAccessBlobPermissions.Read | SharedAccessBlobPermissions.Write | SharedAccessBlobPermissions.Create
-            };
-
-            var sasBlobToken = blob.GetSharedAccessSignature(adHocSAS);
-            return new
-            {
-                url = blob.Uri + sasBlobToken,
-                blobUri = blob.Uri,
-                sasBlobToken
-            };
-        }
+      var sasBlobToken = blob.GetSharedAccessSignature(adHocSAS);
+      return new
+      {
+        url = blob.Uri + sasBlobToken,
+        blobUri = blob.Uri,
+        sasBlobToken
+      };
     }
+  }
 }
